c336415b7c605d9e3c3e8a85d6d2d29afd69cf99
Vidarshan <vidarshanadithya3@gmail.com>
2022-07-28 19:32:23 +0530

    [changed] added seperate form

---
+++ lib/forms/AddToDoForm.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:to_dos/models/Todo.dart';
+import 'package:to_dos/state/todo/actions.dart';
+
+class AddToDoForm extends StatefulWidget {
+  const AddToDoForm({Key? key}) : super(key: key);
+
+  @override
+  State<AddToDoForm> createState() => _AddToDoFormState();
+}
+
+class _AddToDoFormState extends State<AddToDoForm> {
+  final _formKey = GlobalKey<FormState>();
+  late ToDoActions toDoActions = ToDoActions(context: context);
+
+  void addToDo() {
+    ToDo newTodo = ToDo(name: 'Buy Milk', description: 'Buy 4L of milk');
+    toDoActions.addToDo(newTodo);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(children: <Widget>[
+        CupertinoTextFormFieldRow(
+          prefix: const Text('Enter text'),
+          placeholder: 'Enter text',
+          validator: (String? value) {
+            if (value == null || value.isEmpty) {
+              return 'Please enter a value';
+            }
+            return null;
+          },
+        ),
+        CupertinoButton.filled(
+            onPressed: addToDo, child: const Text('Add Todo'))
+      ]),
+    );
+  }
+}
--- lib/screens/AddToDoScreen.dart
+++ lib/screens/AddToDoScreen.dart
@@ -1,30 +1,65 @@
 import 'package:flutter/cupertino.dart';
+import 'package:to_dos/models/Todo.dart';
+import 'package:to_dos/state/todo/actions.dart';
 
 class AddToDoScreen extends StatefulWidget {
-  AddToDoScreen({Key? key}) : super(key: key);
+  const AddToDoScreen({Key? key}) : super(key: key);
 
   @override
   State<AddToDoScreen> createState() => _AddToDoScreenState();
 }
 
 class _AddToDoScreenState extends State<AddToDoScreen> {
+  final myController = TextEditingController();
+  late ToDoActions toDoActions = ToDoActions(context: context);
+
+  @override
+  void initState() {
+    super.initState();
+    myController.addListener(_printLatestValue);
+  }
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  void addToDo() {
+    ToDo newTodo = ToDo(name: 'Buy Milk', description: 'Buy 4L of milk');
+    toDoActions.addToDo(newTodo);
+  }
+
+  void _printLatestValue() {
+    print('Second text field: ${myController.text}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: const CupertinoNavigationBar(middle: Text('Add')),
+      navigationBar:
+          const CupertinoNavigationBar(middle: Text('Create new Todo')),
       child: Container(
         margin: const EdgeInsets.only(left: 20, right: 20),
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              SizedBox(
+            children: [
+              // Row(
+              //   children: const [
+              //     Text(
+              //       'Title',
+              //       textAlign: TextAlign.left,
+              //     )
+              //   ],
+              // ),
+              const SizedBox(
                 height: 40,
                 child: CupertinoTextField(
                   placeholder: 'Todo Title',
                 ),
               ),
-              Padding(
+              const Padding(
                 padding: EdgeInsets.only(top: 20),
                 child: SizedBox(
                   height: 40,
@@ -33,7 +68,14 @@ class _AddToDoScreenState extends State<AddToDoScreen> {
                   ),
                 ),
               ),
-              CupertinoButton(onPressed: null, child: Text('Add Todo'))
+              Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: SizedBox(
+                  width: double.maxFinite,
+                  child: CupertinoButton.filled(
+                      onPressed: addToDo, child: const Text('Add Todo')),
+                ),
+              )
             ]),
       ),
     );
