02804df2910774036e54ba412d1f3402cf663b68
vidarshanadithya <vidarshanadithya3@gmail.com>
2022-08-31 20:24:04 +0530

    [added] new UI for todo info

--- lib/screens/ToDoInfoScreen.dart
+++ lib/screens/ToDoInfoScreen.dart
@@ -63,44 +63,40 @@ class _ToDoInfoScreenState extends State<ToDoInfoScreen> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+        backgroundColor: Color.fromARGB(255, 239, 239, 239),
         navigationBar: const CupertinoNavigationBar(previousPageTitle: 'ToDos'),
-        child: Container(
-          margin: const EdgeInsets.only(left: 20, right: 20),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 40,
-                  child: CupertinoTextField(
-                    placeholder: 'Todo Title',
+        child: SafeArea(
+          child: Container(
+            child: Column(children: [
+              CupertinoFormSection.insetGrouped(
+                header: const Text('TODO INFORMATION'),
+                children: [
+                  CupertinoTextFormFieldRow(
+                    prefix: const Text('Title'),
+                    placeholder: 'ToDo Title',
                     controller: _titleController,
-                    textInputAction: TextInputAction.next,
                     onChanged: (value) => setState(() {
                       title = value;
                     }),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: SizedBox(
-                    height: 40,
-                    child: CupertinoTextField(
-                        controller: _descriptionController,
-                        placeholder: 'Todo Description',
-                        onChanged: (value) => setState(() {
-                              description = value;
-                            })),
+                  CupertinoTextFormFieldRow(
+                    prefix: const Text('Description'),
+                    placeholder: 'ToDo Description',
+                    controller: _descriptionController,
+                    onChanged: (value) => setState(() {
+                      description = value;
+                    }),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Row(
+                ],
+              ),
+              CupertinoFormSection.insetGrouped(
+                header: const Text('TODO STATUS'),
+                children: [
+                  CupertinoFormRow(
+                      child: Row(
                     children: [
                       const Text(
                         'Complete ToDo',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 189, 189, 189)),
                       ),
                       const Spacer(),
                       CupertinoSwitch(
@@ -109,39 +105,49 @@ class _ToDoInfoScreenState extends State<ToDoInfoScreen> {
                                 completed = value;
                               }))
                     ],
+                  ))
+                ],
+              ),
+              CupertinoFormSection.insetGrouped(
+                header: const Text('OTHER INFORMATION'),
+                children: [
+                  CupertinoTextFormFieldRow(
+                    prefix: const Text('Created At'),
+                    initialValue: DateFormat('yyyy-MM-dd kk:mm a').format(
+                        DateTime.fromMillisecondsSinceEpoch(widget.createdAt)),
+                    enabled: false,
                   ),
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Text(
-                      'Created at ${DateFormat('yyyy-MM-dd kk:mm a').format(DateTime.fromMillisecondsSinceEpoch(widget.createdAt))}',
-                      style: const TextStyle(color: Colors.grey, fontSize: 16),
+                  CupertinoTextFormFieldRow(
+                    prefix: const Text('Updated At'),
+                    initialValue: DateFormat('yyyy-MM-dd kk:mm a').format(
+                        DateTime.fromMillisecondsSinceEpoch(widget.createdAt)),
+                    enabled: false,
+                  ),
+                ],
+              ),
+              CupertinoFormSection.insetGrouped(
+                  header: const Text('ACTIONS'),
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      child: CupertinoButton.filled(
+                          onPressed: () => editToDo(widget.id, title,
+                              description, completed, widget.createdAt),
+                          child: const Text('Edit Todo')),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: SizedBox(
-                    width: double.maxFinite,
-                    child: CupertinoButton.filled(
-                        onPressed: () => editToDo(widget.id, title, description,
-                            completed, widget.createdAt),
-                        child: const Text('Edit Todo')),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: SizedBox(
-                    width: double.maxFinite,
-                    child: CupertinoButton(
-                        color: Colors.redAccent,
-                        onPressed: () => deleteToDo(),
-                        child: const Text('Delete Todo')),
-                  ),
-                ),
-              ]),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: CupertinoButton(
+                            color: Colors.redAccent,
+                            onPressed: () => deleteToDo(),
+                            child: const Text('Delete Todo')),
+                      ),
+                    ),
+                  ]),
+            ]),
+          ),
         ));
   }
 }
